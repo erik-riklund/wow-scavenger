462a53b2d76d34d6cd4f51e3ffb1e48c0662812a
Erik R <erik.riklund@outlook.com>
2024-12-04 21:34:31 +0100

    * restructure

--- handler.lua
+++ handler.lua
@@ -15,48 +15,48 @@ local context = select(2, ...)
 --See the GNU General Public License <https://www.gnu.org/licenses/> for more details.
 
 ---@type number
-local item_count
+local itemCount
 
 ---@type number
-local slots_cleared
+local slotsCleared
 
 ---@type Vector
-local remaining_slots
+local remainingSlots
 
----@type table<LOOT_SLOT_TYPE, LootHandler>
+---@type table<LOOTSLOT_TYPE, LootHandler>
 ---Handlers responsible for processing different types of loot slots.
 local handlers =
 {
   ---@type LootHandler
-  [ENUM.LOOT_SLOT_TYPE.ITEM] = function(slot_info)
+  [ENUM.LOOT_SLOT_TYPE.ITEM] = function(slotInfo)
     local filters = context.plugin:getSetting 'FILTERS' --[[@as LootFilters]]
-    local item_info = backbone.getItemInfo (slot_info.link)
+    local itemInfo = backbone.getItemInfo (slotInfo.link)
 
     --Items listed in the ignore filter should not be looted.
-    if filters.IGNORE[item_info.id] then return false end
+    if filters.IGNORE[itemInfo.id] then return false end
 
     --Items listed in the custom loot filter will be looted.
-    if filters.LOOT[item_info.id] then return true end
+    if filters.LOOT[itemInfo.id] then return true end
 
     --Quest items are looted if `LOOT_ALL` is enabled, or if it's the only item that dropped.
-    if slot_info.quest_item then
+    if slotInfo.isQuestItem then
       local options = context.plugin:getSetting 'QUEST' --[[@as QuestLootOptions]]
       return (options.LOOT_ALL or backbone.getLootCount() == 1)
     end
 
     --Poor quality items are looted if they are within the specified minimum and maximum value range.
-    if item_info.quality == ENUM.ITEM_QUALITY.POOR then
+    if itemInfo.quality == ENUM.ITEM_QUALITY.POOR then
       local options = context.plugin:getSetting 'JUNK' --[[@as JunkLootOptions]]
-      return (item_info.sell_price >= options.MINIMUM_VALUE and item_info.sell_price <= options.MAXIMUM_VALUE)
+      return (itemInfo.sellPrice >= options.MINIMUM_VALUE and itemInfo.sellPrice <= options.MAXIMUM_VALUE)
     end
 
     --Tradeskill items are looted if their subtype is listed as lootable or if it's a cooking reagent
     --from fishing (with fishing loot enabled), and the item quality is below the set quality cap.
-    if item_info.type_id == ENUM.ITEM_CLASS.TRADEGOODS then
+    if itemInfo.typeId == ENUM.ITEM_CLASS.TRADEGOODS then
       local options = context.plugin:getSetting 'TRADESKILL' --[[@as TradeskillLootOptions]]
 
-      return (options.LOOTABLE_SUBTYPES[item_info.subtype_id] or (item_info.subtype_id == ENUM.TRADESKILL_SUBTYPE.COOKING
-        and backbone.isFishingLoot() and options.LOOT_FISH)) and item_info.quality < options.QUALITY_CAP
+      return (options.LOOTABLE_SUBTYPES[itemInfo.subtypeId] or (itemInfo.subtypeId == ENUM.TRADESKILL_SUBTYPE.COOKING
+        and backbone.isFishingLoot() and options.LOOT_FISH)) and itemInfo.quality < options.QUALITY_CAP
     end
 
     --Soulbound armor and weapons are looted based on a number of variables.
@@ -65,14 +65,14 @@ local handlers =
     --* Gear from the current expansion is only looted if explicitly enabled (default: disabled).
     --* When `ONLY_KNOWN` is enabled, the item's appearance must be known (default: enabled).
     if
-      (item_info.type_id == ENUM.ITEM_CLASS.ARMOR or item_info.type_id == ENUM.ITEM_CLASS.WEAPON)
-        and item_info.bind_type == ENUM.ITEM_BIND.ON_ACQUIRE
+      (itemInfo.typeId == ENUM.ITEM_CLASS.ARMOR or itemInfo.typeId == ENUM.ITEM_CLASS.WEAPON)
+        and itemInfo.bindType == ENUM.ITEM_BIND.ON_ACQUIRE
     then
       local options = context.plugin:getSetting 'GEAR' --[[@as GearLootOptions]]
 
       if options.ENABLED and UnitLevel 'player' >= options.PLAYER_LEVEL then
-        return (options.CURRENT_EXPANSION or item_info.expansion_id < backbone.currentExpansion)
-            and (not options.ONLY_KNOWN or C_TransmogCollection.PlayerHasTransmogByItemInfo (item_info.link))
+        return (options.CURRENT_EXPANSION or itemInfo.expansionId < backbone.currentExpansion)
+            and (not options.ONLY_KNOWN or C_TransmogCollection.PlayerHasTransmogByItemInfo (itemInfo.link))
       end
     end
 
@@ -97,7 +97,7 @@ local handlers =
     if not options.LOOT_CURRENCIES then return false end
 
     local filters = context.plugin:getSetting 'FILTERS' --[[@as LootFilters]]
-    return not filters.IGNORE[slot_info.currency_id]
+    return not filters.IGNORE[slot_info.currencyId]
   end
 }
 
@@ -109,28 +109,28 @@ context.plugin:registerEventListener(
 
     ---@param autoloot boolean
     callback = function (autoloot)
-      slots_cleared = 0
-      item_count = backbone.getLootCount()
+      slotsCleared = 0
+      itemCount = backbone.getLootCount()
 
-      remaining_slots = new 'Vector'
+      remainingSlots = new 'Vector'
 
-      for index = 1, item_count do
-        local slot_info = backbone.getLootslotInfo (index)
+      for index = 1, itemCount do
+        local slotInfo = backbone.getLootslotInfo (index)
   
-        if not slot_info.locked and slot_info.slot_type ~= ENUM.LOOT_SLOT_TYPE.NONE then
-          if autoloot or (not autoloot and handlers[slot_info.slot_type](slot_info)) then
+        if not slotInfo.locked and slotInfo.slotType ~= ENUM.LOOT_SLOT_TYPE.NONE then
+          if autoloot or (not autoloot and handlers[slotInfo.slotType](slotInfo)) then
             backbone.lootSlot(index) -- loot the current slot.
           else
             ---@class LootableSlot
-            local slot = { index = index, info = slot_info }
+            local slot = { index = index, info = slotInfo }
               
-            remaining_slots:insertElement (slot)
+            remainingSlots:insertElement (slot)
           end
         end
       end
 
-      if remaining_slots:getSize() == item_count then
-        context.plugin:invokeChannelListeners ('LOOT_PROCESSED', remaining_slots)
+      if remainingSlots:getSize() == itemCount then
+        context.plugin:invokeChannelListeners ('LOOT_PROCESSED', remainingSlots)
       end
     end
   }
@@ -142,11 +142,11 @@ context.plugin:registerEventListener(
   {
     identifier = 'LOOT_SLOT_CLEARED',
     callback = function ()
-      slots_cleared = slots_cleared + 1
+      slotsCleared = slotsCleared + 1
 
-      local remaining_slots_count = remaining_slots:getSize()
-      if remaining_slots_count > 0 and item_count - slots_cleared == remaining_slots_count then
-        context.plugin:invokeChannelListeners ('LOOT_PROCESSED', remaining_slots)
+      local remaining_slots_count = remainingSlots:getSize()
+      if remaining_slots_count > 0 and itemCount - slotsCleared == remaining_slots_count then
+        context.plugin:invokeChannelListeners ('LOOT_PROCESSED', remainingSlots)
       end
     end
   }
